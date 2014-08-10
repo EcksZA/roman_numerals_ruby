da4887c8afeba1a872b51ac4ff948b107942145a
Xolani Mfuko <xmfuko@gmail.com>
2014-08-09 17:30:09 -0700

    test for larger number, without 9 or 4 digit in number

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -5,4 +5,7 @@ describe "roman_numeral" do
   it "outputs any number which is less than 10, but not equal to 4 or 9 in roman numerals" do
     roman_numeral(8).should eq("VIII")
   end
+    it "outputs roman numerals for a large number that does not include the digits 4 and 9 in it" do
+    roman_numeral(2563).should eq("MMDLXIII")
+  end
 end
