934aca96f1ec7c42354768cfba265ac7eb8cddb9
Xolani Mfuko <xmfuko@gmail.com>
2014-08-09 17:58:31 -0700

    complete the user interface for the roman numerals program

--- user_interface.rb
+++ user_interface.rb
@@ -1,5 +1,17 @@
-require './lib/roman_numeral'
+require './lib/roman_numerals'
 
 def main_menu
+  puts "Roman Numerals Converter"
+  puts "\n"
+  puts "Enter a number between 1 and 3999 to see its Roman Numeral form"
+  get_number = gets.chomp
 
+  puts "The Roman Numeral for #{get_number} is:"
+  puts "#{roman_numeral(get_number.to_i)}"
+  puts "\n\n"
+  puts "Press 'a' to try another number or any other key to log out"
+  get_option = gets.chomp
+
+  get_option == 'a' ? main_menu : (puts "Goodbye")
 end
+main_menu
